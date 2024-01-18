6d8485cb72a730fee8d5eb70ad380dd0cb305778
SaeedHabibi23 <ghulamsaeed54@gmail.com>
2024-01-18 21:20:29 +0430

    Comment for admin layout

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -5,7 +5,7 @@ import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 
 import { Link } from '@inertiajs/vue3';
 </script>
-
+<!-- This template used for admin panle page -->
 <template>
 <aside class="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
     <div>
